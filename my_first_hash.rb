430ebc40ac5d827df56ec2487dff46217b026995
Susan Meyers <smmeyers24@gmail.com>
2018-08-23 03:42:55 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   hash = {"name" => "Susan", "address" => "New Jersey"}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-
 end
 
 
