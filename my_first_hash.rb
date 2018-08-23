c482322a40e6d15825dc1aa7f2015b7880ef625e
Susan Meyers <smmeyers24@gmail.com>
2018-08-23 03:51:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,8 +16,6 @@ end
 # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
-
-
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
@@ -25,7 +23,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]# Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
 
