681729c8c59e9415e4df1ad8eab376570e00fe41
Susan Meyers <smmeyers24@gmail.com>
2018-08-23 03:49:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,9 +11,9 @@ def shipping_manifest
               "oil paintings" => 3
             } #
 end
-              #set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information:
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+#set a variable called `the_manifest`, equal to a hash
+# fill that hash with key/value pairs that describe the following information:
+# We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
 
