cc5a5ee8d543104d50747ab40cc3ff5f32724fec
Susan Meyers <smmeyers24@gmail.com>
2018-08-23 03:59:55 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                  }
   shipping_manifest["muskets"] = 2
                    ["gun powder"] = 4# add 2 muskets to the shipping_manifest hash below
-            
+
 
   return shipping_manifest
 
